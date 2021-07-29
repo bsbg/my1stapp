1c3b31eb331e5f4d1236c1f8d18e214351468214
bsbg <szq0813@outlook.com>
2021-07-29 14:00:14 +0800

    hola

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 春和 on 2021/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola, guys!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/my1stappApp.swift
@@ -0,0 +1,17 @@
+//
+//  my1stappApp.swift
+//  Shared
+//
+//  Created by 春和 on 2021/7/29.
+//
+
+import SwiftUI
+
+@main
+struct my1stappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
